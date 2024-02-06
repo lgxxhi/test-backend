e966d8e8c9013cd625010e74da8c57e5fd3066fe
lgxxhi <luisgsilva95@gmail.com>
2024-02-06 12:53:22 -0500

    added cors to middleware

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
 const usersController = require("./usersControllers");
+const cors = require("cors");
 
 app.get("/", (req, res) => {
   res.send("This is an app my dude.");
+});
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (request, response) => {
+  response.send("This is an app my friend.");
 });
 
 //create controller
